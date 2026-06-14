36a4785a41c12836ccf8223f45bda3039ceed458
Vector <basu.sagnik0606@gmail.com>
2026-06-14 08:27:51 +0530

    Updated gitignore

--- test-role-nav.js
+++
@@ -1,7 +0,0 @@
-const db = require('./middleware/db');
-async function run() {
-  const [rows] = await db.query("SELECT * FROM role_nav WHERE role = 'pmc_head'");
-  console.log(rows);
-  process.exit(0);
-}
-run();
